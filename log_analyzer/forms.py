85d56da9499306cbd5c8a2fef21037240b3020a3
ARJUN S <arjunevershine@gmail.com>
2023-12-12 11:39:44 +0530

    Pivot to Django and React

--- log_analyzer/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class LogFileUploadForm(forms.Form):
-    log_file = forms.FileField()
-
-class LogFileUploadForm(forms.Form):
-    log_file = forms.FileField(label='Select a log file')
