b5e8511a21a3484f6b1d13451c9f5329ba9b75d4
ARJUN S <arjunevershine@gmail.com>
2023-12-13 23:46:55 +0530

    Mixing Comma fix

--- server/api/urls.py
+++ server/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('viewallunits', views.view_all_units,name='viewallunits'),
     path('viewunitbyid/<int:Id>/',views.view_unit_by_id,name='viewunitbyid'),
     path('viewalldevices',views.view_all_devices,name='viewalldevices'),
-    path('viewdevicebyid/<int:Id>',views.view_device_by_id,name='viewdevicebyid')
+    path('viewdevicebyid/<int:Id>',views.view_device_by_id,name='viewdevicebyid'),
     path('save_log_line/', views.save_log_line, name='save_log_line'),
 ]
