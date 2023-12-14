dd85341feab9f1ace121794a1f397b0cd24038f0
V Nivas Chowdary <sunnysnivas@gmail.com>
2023-12-14 11:12:35 +0530

    Added Creation date to all tables

--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,44 @@
-# myapi/views.py
-from django.contrib.auth.models import User
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .models import LogLines, CrpfDevice, ThreatInfo, Alerts
+from rest_framework import status
 import re
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+from .models import *
+
+
+@api_view(['GET'])
+def view_all_units(request):
+    all_units = list(CrpfUnit.objects.all().values())
+    return Response(all_units)
+
+
+@api_view(['GET'])
+def view_unit_by_id(request, Id):
+    unit = list(CrpfUnit.objects.filter(id=Id).values())
+    if unit == []:
+        return Response({"message": "Unit Id not Found"}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(unit)
+
+
+@api_view(['GET'])
+def view_all_devices(request):
+    all_devices = list(CrpfDevice.objects.all().values())
+    return Response(all_devices)
+
+@api_view(['GET'])
+def view_device_by_id(request,Id):
+    device = list(CrpfDevice.objects.filter(id=Id).values())
+    if device == []:
+        return Response({"message": "Device Id not Found"}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(device)
+
 @api_view(['POST'])
 def save_log_line(request):
     crpf_device_name = request.data.get('crpf_device_name')
@@ -32,3 +66,21 @@ def save_log_line(request):
 
 
 
+@api_view(['POST'])
+# @permission_classes([IsAdminUser])
+def set_assignee(request):
+
+    alert_id = request.data.get('alert_id')
+    username = request.data.get('username')
+    try:
+        alert_instance = Alerts.objects.get(id=alert_id)
+    except Alerts.DoesNotExist:
+        return Response({'message': 'Alerts instance not found'}, status=404)
+    try:
+        assignee_user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({'message': 'User not found'}, status=404)
+    alert_instance.assignee = assignee_user
+    alert_instance.save()
+
+    return Response({'message': 'Assignee set successfully'})
