95515e2d8836eac0bcdc88a524ed2a28837892a7
V Nivas Chowdary <sunnysnivas@gmail.com>
2023-12-14 11:18:10 +0530

    Resolved error

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,11 +3,8 @@ from django.urls import re_path,path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
 path('viewallunits', views.view_all_units,name='viewallunits'),
-=======
     path('viewallunits', views.view_all_units,name='viewallunits'),
->>>>>>> b5e8511a21a3484f6b1d13451c9f5329ba9b75d4
     path('viewunitbyid/<int:Id>/',views.view_unit_by_id,name='viewunitbyid'),
     path('viewalldevices',views.view_all_devices,name='viewalldevices'),
     path('viewdevicebyid/<int:Id>',views.view_device_by_id,name='viewdevicebyid'),
