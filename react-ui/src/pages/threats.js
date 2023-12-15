03f0ac3ec5cdb74aa8ba32dac4901ab5a67464d8
ARJUN S <arjunevershine@gmail.com>
2023-12-15 11:36:02 +0530

    Threats render

--- react-ui/src/pages/threats.js
+++ react-ui/src/pages/threats.js
@@ -1,148 +1,129 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import { useCallback, useMemo, useState, useEffect } from 'react';
+import Head from 'next/head';
+import ArrowDownOnSquareIcon from '@heroicons/react/24/solid/ArrowDownOnSquareIcon';
+import ArrowUpOnSquareIcon from '@heroicons/react/24/solid/ArrowUpOnSquareIcon';
+import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
+import { Box, Button, Container, Stack, SvgIcon, Typography } from '@mui/material';
+import { useSelection } from 'src/hooks/use-selection';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import { ThreatsTable, threatsTable } from 'src/sections/threats/threatsTable';
+import { ThreatsSearch, threatsSearch } from 'src/sections/threats/threatsSearch';
 import axios from 'axios';
 import { API_SERVER } from 'src/config/constant';
-import {
-  Avatar,
-  Box,
-  Card,
-  Checkbox,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TablePagination,
-  TableRow,
-} from '@mui/material';
-import { Scrollbar } from 'src/components/scrollbar';
+const Page = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [threats, setthreats] = useState([]);
+  const threatsIds = useMemo(() => threats.map((threat) => threat.id), [threats]);
+  const threatsSelection = useSelection(threatsIds);
 
-const ThreatsTable = (props) => {
-  const {
-    count = 0,
-    onDeselectAll,
-    onDeselectOne,
-    onPageChange = () => {},
-    onRowsPerPageChange,
-    onSelectAll,
-    onSelectOne,
-    page = 0,
-    rowsPerPage = 0,
-    selected = [],
-  } = props;
+  const handlePageChange = useCallback((event, value) => {
+    setPage(value);
+  }, []);
 
-  const [threats, setThreats] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const handleRowsPerPageChange = useCallback((event) => {
+    setRowsPerPage(event.target.value);
+  }, []);
 
   useEffect(() => {
     const fetchThreats = async () => {
       try {
-        const response = await axios.get(API_SERVER + 'viewallthreats');
-        setThreats(response.data);
+        const response = await axios.get(API_SERVER+'view_all_threats');
+        setthreats(response.data);
       } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+        console.error('Error fetching threats:', error);
       }
     };
 
     fetchThreats();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  const selectedSome = selected.length > 0 && selected.length < threats.length;
-  const selectedAll = threats.length > 0 && selected.length === threats.length;
-
   return (
-    <Card>
-      <Scrollbar>
-        <Box sx={{ minWidth: 800 }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell padding="checkbox">
-                  <Checkbox
-                    checked={selectedAll}
-                    indeterminate={selectedSome}
-                    onChange={(event) => {
-                      if (event.target.checked) {
-                        onSelectAll?.();
-                      } else {
-                        onDeselectAll?.();
-                      }
-                    }}
-                  />
-                </TableCell>
-                <TableCell>Threat Name</TableCell>
-                <TableCell>Threat Description</TableCell>
-                <TableCell>Threat Pattern</TableCell>
-                <TableCell>Threat Score</TableCell>
-                <TableCell>Reference Links</TableCell>
-                <TableCell>Creation Time</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {threats.map((threat) => {
-                const isSelected = selected.includes(threat.id);
-
-                return (
-                  <TableRow hover key={threat.id} selected={isSelected}>
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={isSelected}
-                        onChange={(event) => {
-                          if (event.target.checked) {
-                            onSelectOne?.(threat.id);
-                          } else {
-                            onDeselectOne?.(threat.id);
-                          }
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell>{threat.name}</TableCell>
-                    <TableCell>{threat.description}</TableCell>
-                    <TableCell>{threat.signature}</TableCell>
-                    <TableCell>{threat.score}</TableCell>
-                    <TableCell>{threat.ref_links}</TableCell>
-                    <TableCell>{threat.creation_time}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </Box>
-      </Scrollbar>
-      <TablePagination
-        component="div"
-        count={count}
-        onPageChange={onPageChange}
-        onRowsPerPageChange={onRowsPerPageChange}
-        page={page}
-        rowsPerPage={rowsPerPage}
-        rowsPerPageOptions={[5, 10, 25]}
-      />
-    </Card>
+    <>
+      <Head>
+        <title>Threats | Logalyzer</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              spacing={4}
+            >
+              <Stack spacing={1}>
+                <Typography variant="h4">Threats</Typography>
+                <Stack
+                  alignItems="center"
+                  direction="row"
+                  spacing={1}
+                >
+                  <Button
+                    color="inherit"
+                    startIcon={(
+                      <SvgIcon fontSize="small">
+                        <ArrowUpOnSquareIcon />
+                      </SvgIcon>
+                    )}
+                  >
+                    Import
+                  </Button>
+                  <Button
+                    color="inherit"
+                    startIcon={(
+                      <SvgIcon fontSize="small">
+                        <ArrowDownOnSquareIcon />
+                      </SvgIcon>
+                    )}
+                  >
+                    Export
+                  </Button>
+                </Stack>
+              </Stack>
+              <div>
+                <Button
+                  startIcon={(
+                    <SvgIcon fontSize="small">
+                      <PlusIcon />
+                    </SvgIcon>
+                  )}
+                  variant="contained"
+                >
+                  Add
+                </Button>
+              </div>
+            </Stack>
+            <ThreatsSearch />
+            <ThreatsTable
+              count={threats.length}
+              items={threats}
+              onDeselectAll={threatsSelection.handleDeselectAll}
+              onDeselectOne={threatsSelection.handleDeselectOne}
+              onPageChange={handlePageChange}
+              onRowsPerPageChange={handleRowsPerPageChange}
+              onSelectAll={threatsSelection.handleSelectAll}
+              onSelectOne={threatsSelection.handleSelectOne}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              selected={threatsSelection.selected}
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
   );
 };
 
-ThreatsTable.propTypes = {
-  count: PropTypes.number,
-  onDeselectAll: PropTypes.func,
-  onDeselectOne: PropTypes.func,
-  onPageChange: PropTypes.func,
-  onRowsPerPageChange: PropTypes.func,
-  onSelectAll: PropTypes.func,
-  onSelectOne: PropTypes.func,
-  page: PropTypes.number,
-  rowsPerPage: PropTypes.number,
-  selected: PropTypes.array,
-};
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 
-export default ThreatsTable;
+export default Page;
